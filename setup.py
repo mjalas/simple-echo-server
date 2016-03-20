79ddc51379d6374414cda55498ec6aea24884547
Mats Jalas <mats.jalas@gmail.com>
2016-03-20 21:00:26 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,78 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = ""
+if path.exists(path.join(here, 'README.rst')):
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='simple-echo-server',
+    version='0.0.1',
+
+    description='A simple echo server application',
+    long_description=long_description,
+    url='https://github.com/mjalas/simple-echo-server',
+
+    # Author details
+    author='Mats Jalas',
+    author_email='',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Network application testing',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='echo-server network-testing',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'env_creators']),
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #    'dev': ['check-manifest'],
+    #    'test': ['coverage'],
+    # },
+
+    entry_points={
+        'console_scripts': [
+            'echo-server=app:main',
+        ],
+    },
+)
